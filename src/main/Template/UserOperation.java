5d3a60bc3f6949249cb3ff3a905f79d6eac2ec82
Ian Wen <h_wen@ucsb.edu>
2023-12-07 00:53:13 -0800

    implemented getstockaccountsaid

--- src/main/Template/UserOperation.java
+++ src/main/Template/UserOperation.java
@@ -6,6 +6,7 @@ import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public abstract class UserOperation {
     protected OracleConnection connection;
@@ -49,6 +50,24 @@ public abstract class UserOperation {
         ) {
             resultSet.next();
             return resultSet.getBoolean("boolValue");
+        }
+    }
+
+    public ArrayList<Integer> getStockAccountsAid(String username, Statement statement) throws SQLException {
+        ArrayList<Integer> out = new ArrayList<Integer>();
+        try (
+            ResultSet resultSet = statement.executeQuery(
+                "SELECT aid " +
+                "FROM StockAccounts NATURAL JOIN Accounts " +
+                "WHERE uname = '" + username + "'"
+            )
+        ) {
+            while (resultSet.next()) {
+                int resultAid = resultSet.getInt("aid");
+                out.add(resultAid);
+            }
+
+            return out;
         }
     }
 }
