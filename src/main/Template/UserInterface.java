92a55dbabb74383320b1086c060f4052942594c8
Ian Wen <h_wen@ucsb.edu>
2023-12-06 21:45:17 -0800

    update userinterface template

--- src/main/Template/UserInterface.java
+++ src/main/Template/UserInterface.java
@@ -1,23 +1,29 @@
 package main.Template;
 
+import oracle.jdbc.OracleConnection;
+
 import java.util.Scanner;
 
 public abstract class UserInterface {
     private static String options = "User interface template";
     protected static Scanner myObj = new Scanner(System.in);;
     
-    public static void display() {
-        System.out.println(options);
-        String userChoice = myObj.nextLine();
-        switch (userChoice) {
-            case "1":
-                System.out.println("1st choice");
-                break;
-            case "0":
-                System.out.println("cancel/exit");
-                break;
-            default:
-                System.out.println("Please enter a number between 0 and 1 (no leading zeros)");
+    public static void display(OracleConnection connection) {
+        String userChoice = "";
+
+        while (!userChoice.equals("0")) {
+            System.out.print(options);
+            userChoice = myObj.nextLine();
+            switch (userChoice) {
+                case "1":
+                    System.out.println("1st choice");
+                    break;
+                case "0":
+                    System.out.println("cancel/exit");
+                    break;
+                default:
+                    System.out.println("Please enter a number between 0 and 1 (no leading zeros)");
+            }
         }
         myObj.close();
     }
